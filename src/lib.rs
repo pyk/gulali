b4b4d9ce138b3633c2ccf483fa0d66375714cb60
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2019-03-13 01:38:52 +0700

    Use simple approach

--- src/lib.rs
+++ src/lib.rs
@@ -47,162 +47,16 @@
 //! [Rust book]: https://doc.rust-lang.org/book/
 //!
 //! ### The Basics
-//! Crabsformer's main data structure is the homogeneous multidimensional [vector].
-//! It is a table of elements (usually numbers), all of the same type,
-//! indexed by a tuple of positive integers.
+//! Crabsformer's main data structures are [`Vector<T>`] and [`Matrix<T>`].
+//! You can build vector and matrix
+//! you can perform opertaion on them
+//! you can
 //!
-//! [vector]: https://doc.rust-lang.org/std/vec/struct.Vec.html
-//!
-//! For example, the coordinates of a point in 3D space `[1, 2, 1]` has
-//! one dimension. That dimension has 3 elements in it, so we say
-//! it has a length of 3. In the example pictured below,
-//! the vector has 2 dimensions. The first dimension has a length of 2,
-//! the second dimension has a length of 3.
-//!
-//! ```ignore
-//! [
-//!     [ 1., 0., 0.],
-//!     [ 0., 1., 2.]
-//! ]
-//! ```
-//!
-//! Crabsformer uses Rust's [vector] standard data structure extensively.
-//! Crabsformer don't reinvent yet-another data strucutre to keep things
-//! simple and easy to use. Crabsformer add attributes to Rust's [vector]
-//! like the following:
-//!
-//! - [`dim()`]: the number of dimensions of the vector.
-//! - [`shape()`]: This is a list of integers indicating the
-//!   size of the vector in each dimension.
-//!   For a matrix with `n` rows and `m` columns, shape will be `[n,m]`.
-//!   The length of the shape is therefore the number of
-//!   dimensions, [`dim()`].
-//! - [`size()`]: the total number of elements of the vector.
-//!   This is equal to the product of the elements of shape.
-//!
-//! [slice]: https://doc.rust-lang.org/rust-by-example/primitives/array.html
-//! [`dim()`]: attributes/trait.Dimension.html#tymethod.dim
-//! [`shape()`]: attributes/trait.Shape.html#tymethod.shape
-//! [`size()`]: attributes/trait.Size.html#tymethod.size
+//! [`Vector<T>`]: struct.Vector.html
 //!
 //! ### An Example
-//! ```rust
-//! # use crabsformer::prelude::*;
-//! // Generate two-dimensional vector with shape [3, 3]
-//! // filled with zeros; i32 can be changed into any
-//! // numeric data types.
-//! let matrix: Vec<Vec<i32>> = Vec::two_dim()
-//!     .with_shape([3, 3])
-//!     .zeros()
-//!     .generate();
-//!
-//! assert_eq!(matrix.dim(), 2);
-//! assert_eq!(matrix.shape(), [3, 3]);
-//! assert_eq!(matrix.size(), 9);
-//! ```
 //!
 //! ### Vector Creation
-//! There are several ways to create vectors.
-//!
-//! For example, you can create a vector using [`vec!`][vec!] macro
-//! like the following:
-//!
-//! ```rust
-//! # use crabsformer::prelude::*;
-//! let a = vec![1, 2, 3];
-//! assert_eq!(a.dim(), 1);
-//! assert_eq!(a.shape(), [3]);
-//! assert_eq!(a.size(), 3);
-//!
-//! let b = vec![
-//!     vec![1, 2, 3],
-//!     vec![4, 5, 6],
-//!     vec![8, 9, 10],
-//! ];
-//! assert_eq!(b.dim(), 2);
-//! assert_eq!(b.shape(), [3, 3]);
-//! assert_eq!(b.size(), 9);
-//! ```
-//!
-//! [vec!]: https://doc.rust-lang.org/std/macro.vec.html
-//!
-//! The type of the resulting vector is deduced from the type
-//! of the elements in the macro.
-//!
-//! Often, the elements of a vector are originally unknown, but
-//! its shape is known. Hence, Crabsformer offers several functions to
-//! create vectors with initial placeholder content. These minimize
-//! the necessity of growing vectors, an expensive operation.
-//!
-//! [`one_dim()`] will create a one-dimensional vector with specified
-//! shape and values. For example:
-//!
-//! ```
-//! # use crabsformer::prelude::*;
-//! // Generate one-dimensional vector with shape [5]
-//! // filled with zeros; f64 can be changed into any
-//! // numeric data types.
-//! let bias: Vec<f64> = Vec::one_dim()
-//!     .with_shape([5])
-//!     .zeros()
-//!     .generate();
-//!
-//! assert_eq!(bias, [0.0, 0.0, 0.0, 0.0, 0.0]);
-//! ```
-//!
-//! [`two_dim()`] will create a two-dimensional vector with specified
-//! shape and values. For example:
-//!
-//! ```
-//! # use crabsformer::prelude::*;
-//! // Generate two-dimensional vector with shape [2, 2]
-//! // filled with ones; f64 can be changed into any
-//! // numeric data types.
-//! let matrix: Vec<Vec<f64>> = Vec::two_dim()
-//!     .with_shape([2, 2])
-//!     .ones()
-//!     .generate();
-//!
-//! assert_eq!(matrix, [[1.0, 1.0], [1.0, 1.0]]);
-//! ```
-//!
-//! [`three_dim()`] will create a three-dimensional vector with specified
-//! shape and values. For example:
-//!
-//! ```
-//! # use crabsformer::prelude::*;
-//! // Generate three-dimensional vector with shape [1, 1, 2]
-//! // filled with 5.0; f64 can be changed into any
-//! // numeric data types.
-//! let test: Vec<Vec<Vec<f64>>> = Vec::three_dim()
-//!     .with_shape([1, 1, 2])
-//!     .full_of(5.0)
-//!     .generate();
-//!
-//! assert_eq!(test, [[[5.0, 5.0]]]);
-//! ```
-//!
-//! [`four_dim()`] will create a four-dimensional vector with specified
-//! shape and values. For example:
-//!
-//! ```
-//! # use crabsformer::prelude::*;
-//! // Generate a four-dimensional vector with shape [1, 1, 1, 2]
-//! // filled with ones; f64 can be changed into any
-//! // numeric data types.
-//! let test: Vec<Vec<Vec<Vec<f64>>>> = Vec::four_dim()
-//!     .with_shape([1, 1, 1, 2])
-//!     .ones()
-//!     .generate();
-//!
-//! assert_eq!(test, [[[[1.0, 1.0]]]]);
-//! ```
-//!
-//! [`one_dim()`]: builders/trait.OneDimensional.html#tymethod.one_dim
-//! [`two_dim()`]: builders/trait.TwoDimensional.html#tymethod.two_dim
-//! [`three_dim()`]: builders/trait.ThreeDimensional.html#tymethod.three_dim
-//! [`four_dim()`]: builders/trait.FourDimensional.html#tymethod.four_dim
-//!
 //!
 //! ## Getting help
 //! Feel free to start discussion at [GitHub issues].
@@ -220,9 +74,11 @@
 //! [Apache-2.0]: https://github.com/pyk/crabsformer/blob/master/LICENSE
 //!
 extern crate num;
+extern crate rand;
 
-pub mod builders;
-
-pub mod attributes;
-
+mod matrix;
 pub mod prelude;
+mod vector;
+
+pub use matrix::*;
+pub use vector::*;
---
+++ src/vector.rs
@@ -0,0 +1,358 @@
+// Copyright (c) 2019, Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use num::{Float, FromPrimitive, Num};
+use rand::distributions::uniform::SampleUniform;
+use rand::distributions::{Distribution, Normal, Uniform};
+use std::fmt::Display;
+use std::ops::AddAssign;
+
+/// Vector data structure
+///
+/// TODO: add overview about vector here.
+/// 1. how to create a vector
+/// 2. Vector operation
+/// 3. Indexing, etc.
+#[derive(Debug)]
+pub struct Vector<T> {
+    /// Vector size
+    size: usize,
+    data: Vec<T>,
+}
+
+impl<T> Vector<T> {
+    pub fn size(&self) -> usize {
+        self.size
+    }
+}
+
+// NOTE:
+// - vector is immutable data type
+impl<T> Vector<T>
+where
+    T: FromPrimitive + Num + Copy,
+{
+    pub fn full(size: usize, value: T) -> Vector<T> {
+        // Initialize and populate the vector with specified value
+        let data = vec![value; size];
+        Vector {
+            size: size,
+            data: data,
+        }
+    }
+
+    pub fn zeros(size: usize) -> Vector<T> {
+        Self::full(size, T::from_i32(0).unwrap())
+    }
+
+    pub fn zeros_like(v: &Vector<T>) -> Vector<T> {
+        Self::full(v.size, T::from_i32(0).unwrap())
+    }
+
+    pub fn ones(size: usize) -> Vector<T> {
+        Self::full(size, T::from_i32(1).unwrap())
+    }
+
+    pub fn ones_like(v: &Vector<T>) -> Vector<T> {
+        Self::full(v.size, T::from_i32(1).unwrap())
+    }
+}
+
+impl<U> Vector<U>
+where
+    U: SampleUniform,
+{
+    pub fn uniform(size: usize, low: U, high: U) -> Vector<U> {
+        let mut data = Vec::with_capacity(size);
+        let uniform_distribution = Uniform::new(low, high);
+        // Populate the vector with the default value
+        let mut rng = rand::thread_rng();
+        for _ in 0..size {
+            data.push(uniform_distribution.sample(&mut rng));
+        }
+
+        Vector { size, data }
+    }
+}
+
+impl Vector<f64> {
+    pub fn normal(size: usize, mean: f64, std_dev: f64) -> Vector<f64> {
+        let mut data = Vec::with_capacity(size);
+        let normal_distribution = Normal::new(mean, std_dev);
+        // Populate the vector with the default value
+        let mut rng = rand::thread_rng();
+        for _ in 0..size {
+            data.push(normal_distribution.sample(&mut rng));
+        }
+
+        Vector { size, data }
+    }
+}
+
+impl<F> Vector<F>
+where
+    F: Num + FromPrimitive + Copy + PartialOrd + AddAssign + Display,
+{
+    pub fn range(start: F, stop: F, step: F) -> Vector<F> {
+        // If interval is invalid; then panic
+        if start >= stop {
+            panic!("Invalid range interval start={} stop={}", start, stop)
+        }
+        let mut data = Vec::new();
+        let mut current_step = start;
+        while current_step < stop {
+            data.push(current_step);
+            current_step += step;
+        }
+        Vector {
+            size: data.len(),
+            data,
+        }
+    }
+
+    pub fn linspace(size: usize, start: F, stop: F) -> Vector<F> {
+        // Panics if start >= stop, it should be start < stop
+        if start >= stop {
+            panic!("Invalid linspace interval start={} stop={}", start, stop)
+        }
+        // Convert size to float type
+        let divisor = F::from_usize(size).unwrap();
+        let mut data = Vec::with_capacity(size);
+        let mut current_step = start;
+        let step = (stop - start) / (divisor - F::from_f32(1.0).unwrap());
+        while current_step < stop {
+            data.push(current_step);
+            current_step += step;
+        }
+
+        // Include the `stop` value in the generated sequences
+        if data.len() == size {
+            data[size - 1] = stop;
+        } else {
+            data.push(stop);
+        }
+
+        Vector { size, data }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_full() {
+        let a = Vector::full(5, 5.0);
+        assert_eq!(a.data, [5.0, 5.0, 5.0, 5.0, 5.0]);
+
+        let b = Vector::full(5, 2);
+        assert_eq!(b.data, [2, 2, 2, 2, 2]);
+    }
+
+    #[test]
+    fn test_zeros() {
+        let vf1: Vector<f64> = Vector::zeros(5);
+        assert_eq!(vf1.data, [0.0, 0.0, 0.0, 0.0, 0.0]);
+
+        let vf2: Vector<f32> = Vector::zeros(5);
+        assert_eq!(vf2.data, [0.0, 0.0, 0.0, 0.0, 0.0]);
+
+        let vs1: Vector<usize> = Vector::zeros(5);
+        assert_eq!(vs1.data, [0, 0, 0, 0, 0]);
+
+        let vu1: Vector<u8> = Vector::zeros(5);
+        assert_eq!(vu1.data, [0, 0, 0, 0, 0]);
+
+        let vu2: Vector<u16> = Vector::zeros(5);
+        assert_eq!(vu2.data, [0, 0, 0, 0, 0]);
+
+        let vu3: Vector<u32> = Vector::zeros(5);
+        assert_eq!(vu3.data, [0, 0, 0, 0, 0]);
+
+        let vu4: Vector<u64> = Vector::zeros(5);
+        assert_eq!(vu4.data, [0, 0, 0, 0, 0]);
+
+        let vu5: Vector<u128> = Vector::zeros(5);
+        assert_eq!(vu5.data, [0, 0, 0, 0, 0]);
+
+        let vi1: Vector<i8> = Vector::zeros(5);
+        assert_eq!(vi1.data, [0, 0, 0, 0, 0]);
+
+        let vi2: Vector<i16> = Vector::zeros(5);
+        assert_eq!(vi2.data, [0, 0, 0, 0, 0]);
+
+        let vi3: Vector<i32> = Vector::zeros(5);
+        assert_eq!(vi3.data, [0, 0, 0, 0, 0]);
+
+        let vi4: Vector<i64> = Vector::zeros(5);
+        assert_eq!(vi4.data, [0, 0, 0, 0, 0]);
+
+        let vi5: Vector<i128> = Vector::zeros(5);
+        assert_eq!(vi5.data, [0, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_zeros_like() {
+        let vi1: Vector<i32> = Vector::ones(5);
+        let vi2 = Vector::zeros_like(&vi1);
+        assert_eq!(vi1.size, vi2.size);
+    }
+
+    #[test]
+    fn test_ones() {
+        let vf1: Vector<f64> = Vector::ones(5);
+        assert_eq!(vf1.data, [1.0, 1.0, 1.0, 1.0, 1.0]);
+
+        let vf2: Vector<f32> = Vector::ones(5);
+        assert_eq!(vf2.data, [1.0, 1.0, 1.0, 1.0, 1.0]);
+
+        let vs1: Vector<usize> = Vector::ones(5);
+        assert_eq!(vs1.data, [1, 1, 1, 1, 1]);
+
+        let vu1: Vector<u8> = Vector::ones(5);
+        assert_eq!(vu1.data, [1, 1, 1, 1, 1]);
+
+        let vu2: Vector<u16> = Vector::ones(5);
+        assert_eq!(vu2.data, [1, 1, 1, 1, 1]);
+
+        let vu3: Vector<u32> = Vector::ones(5);
+        assert_eq!(vu3.data, [1, 1, 1, 1, 1]);
+
+        let vu4: Vector<u64> = Vector::ones(5);
+        assert_eq!(vu4.data, [1, 1, 1, 1, 1]);
+
+        let vu5: Vector<u128> = Vector::ones(5);
+        assert_eq!(vu5.data, [1, 1, 1, 1, 1]);
+
+        let vi1: Vector<i8> = Vector::ones(5);
+        assert_eq!(vi1.data, [1, 1, 1, 1, 1]);
+
+        let vi2: Vector<i16> = Vector::ones(5);
+        assert_eq!(vi2.data, [1, 1, 1, 1, 1]);
+
+        let vi3: Vector<i32> = Vector::ones(5);
+        assert_eq!(vi3.data, [1, 1, 1, 1, 1]);
+
+        let vi4: Vector<i64> = Vector::ones(5);
+        assert_eq!(vi4.data, [1, 1, 1, 1, 1]);
+
+        let vi5: Vector<i128> = Vector::ones(5);
+        assert_eq!(vi5.data, [1, 1, 1, 1, 1]);
+    }
+
+    #[test]
+    fn test_ones_like() {
+        let vi1: Vector<i32> = Vector::ones(10);
+        let vi2 = Vector::ones_like(&vi1);
+        assert_eq!(vi1.size, vi2.size);
+    }
+
+    #[test]
+    fn test_uniform() {
+        let vf1: Vector<f32> = Vector::uniform(5, 0.0, 1.0);
+        for value in vf1.data.iter() {
+            assert!((0.0 <= *value) && (*value < 1.0));
+        }
+
+        let vf2: Vector<f64> = Vector::uniform(5, 0.0, 1.0);
+        for value in vf2.data.iter() {
+            assert!((0.0 <= *value) && (*value < 1.0));
+        }
+
+        let vs1: Vector<usize> = Vector::uniform(5, 1, 10);
+        for value in vs1.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vu1: Vector<u8> = Vector::uniform(5, 1, 10);
+        for value in vu1.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vu2: Vector<u16> = Vector::uniform(5, 1, 10);
+        for value in vu2.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vu3: Vector<u32> = Vector::uniform(5, 1, 10);
+        for value in vu3.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vu4: Vector<u64> = Vector::uniform(5, 1, 10);
+        for value in vu4.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vu5: Vector<u128> = Vector::uniform(5, 1, 10);
+        for value in vu5.data.iter() {
+            assert!((1 <= *value) && (*value < 10));
+        }
+
+        let vi1: Vector<i8> = Vector::uniform(5, -10, 10);
+        for value in vi1.data.iter() {
+            assert!((-10 <= *value) && (*value < 10));
+        }
+
+        let vi2: Vector<i16> = Vector::uniform(5, -10, 10);
+        for value in vi2.data.iter() {
+            assert!((-10 <= *value) && (*value < 10));
+        }
+
+        let vi3: Vector<i32> = Vector::uniform(5, -10, 10);
+        for value in vi3.data.iter() {
+            assert!((-10 <= *value) && (*value < 10));
+        }
+
+        let vi4: Vector<i64> = Vector::uniform(5, -10, 10);
+        for value in vi4.data.iter() {
+            assert!((-10 <= *value) && (*value < 10));
+        }
+
+        let vi5: Vector<i128> = Vector::uniform(5, -10, 10);
+        for value in vi5.data.iter() {
+            assert!((-10 <= *value) && (*value < 10));
+        }
+    }
+
+    #[test]
+    fn test_normal() {
+        let a = Vector::normal(5, 2.0, 4.0);
+        let b = Vector::normal(5, 2.0, 4.0);
+        assert_eq!(a.size, b.size);
+        assert_ne!(a.data, b.data);
+    }
+
+    #[test]
+    fn test_range() {
+        // Integer type
+        let a = Vector::range(0, 10, 1);
+        assert_eq!(a.data, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        // Float type
+        let a = Vector::range(0.0, 3.0, 0.5);
+        assert_eq!(a.data, [0.0, 0.5, 1.0, 1.5, 2.0, 2.5]);
+    }
+
+    #[test]
+    fn test_linspace() {
+        // Integer type
+        let a = Vector::linspace(5, 1, 10);
+        assert_eq!(a.data, [1, 3, 5, 7, 10]);
+
+        // Float type
+        let a = Vector::linspace(3, 3.0, 4.0);
+        assert_eq!(a.data, [3.0, 3.5, 4.0]);
+    }
+}
